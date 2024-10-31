ad3399960e89d36c01e0df90e6c6cc11c9449591
Hyunuk Shin <shu914@naver.com>
2024-10-31 19:29:38 +0900

    헤더파일 추가

---
+++ BE/src/auth/auth.h
@@ -0,0 +1,8 @@
+//
+// Created by 신현욱 on 2024. 10. 31..
+//
+
+#ifndef SERVER_PROJECT_AUTH_H
+#define SERVER_PROJECT_AUTH_H
+
+#endif //SERVER_PROJECT_AUTH_H
---
+++ BE/src/chat/chat.h
@@ -0,0 +1,8 @@
+//
+// Created by 신현욱 on 2024. 10. 31..
+//
+
+#ifndef SERVER_PROJECT_CHAT_H
+#define SERVER_PROJECT_CHAT_H
+
+#endif //SERVER_PROJECT_CHAT_H
